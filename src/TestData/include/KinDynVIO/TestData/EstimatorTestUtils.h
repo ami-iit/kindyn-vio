a896887b5fe760130fc328ae32e186ca6c243df7
Prashanth <prashanth.ramadoss@iit.it>
2021-11-04 17:37:10 +0100

    Update EstimatorUtils

--- src/TestData/include/KinDynVIO/TestData/EstimatorTestUtils.h
+++ src/TestData/include/KinDynVIO/TestData/EstimatorTestUtils.h
@@ -40,16 +40,27 @@ enum class EstimatorType
 struct Loggable
 {
     Eigen::VectorXd simTime;
+    Eigen::MatrixXd simVelocity;
     Eigen::MatrixXd simPosition;
     Eigen::MatrixXd simRotation;
+    Eigen::MatrixXd simCOM;
+    Eigen::MatrixXd simCOMVel;
+    Eigen::MatrixXd simAngMomentum;
 
     Eigen::VectorXd estTime;
+    Eigen::MatrixXd estVelocity;
     Eigen::MatrixXd estPosition;
     Eigen::MatrixXd estRotation;
 
     Eigen::VectorXd smootherTime;
+    Eigen::MatrixXd smootherLinearVelocity;
     Eigen::MatrixXd smootherPosition;
     Eigen::MatrixXd smootherRotation;
+
+    Eigen::MatrixXd smootherCOM;
+    Eigen::MatrixXd smootherCOMVel;
+    Eigen::MatrixXd smootherAngMomentum;
+    Eigen::MatrixXd smootherCDMBias;
 };
 
 struct RobotMatData
@@ -69,6 +80,9 @@ struct RobotMatData
     int nrJoints;
     int nrImgs;
     std::vector<std::string> jointsList;
+
+    // update internal
+    Eigen::MatrixXd sbaseVel;
 };
 
 double deg2rad(const double& ang);
@@ -77,6 +91,40 @@ bool readRobotMatFile(const std::string& matFilePath, RobotMatData& rmd);
 bool getAllArucoIMUImageNamesFromDirectory(const std::string& dirPath, std::vector<std::string>& vec, std::string extension = ".jpg");
 bool getArucoIMUImageNames(const std::string& dirPath, const std::string& textFilePath, std::vector<std::string>& vec);
 
+template <typename EigenType>
+bool checkAndWriteToFile(const std::string& varName,
+                         const EigenType& data,
+                         matioCpp::File& file)
+{
+    if (std::is_same_v<EigenType, Eigen::VectorXd>)
+    {
+        if (data.size() < 1)
+        {
+            // skip writing
+            std::cout << "Skipped writing "
+                      << varName << " to file."
+                      << std::endl;
+            return true;
+        }
+    }
+    else if (std::is_same_v<EigenType, Eigen::MatrixXd>)
+    {
+        if (data.rows() < 1)
+        {
+            // skip writing
+            std::cout << "Skipped writing "
+                      << varName << " to file."
+                      << std::endl;
+            return true;
+        }
+    }
+
+    matioCpp::MultiDimensionalArray<double> out{varName,
+                                            {static_cast<std::size_t>(data.rows()),
+                                             static_cast<std::size_t>(data.cols())},
+                                            data.data()};
+    return file.write(out);
+}
 
 bool writeLoggableToFile(const Loggable& log, const std::string& fileName);
 
@@ -97,6 +145,7 @@ ParamsHandler getKinematicInertialWrapperConfig(const EstimatorType& type,
 ParamsHandler getArucoConfig();
 ParamsHandler getIMUPreintConfig();
 ParamsHandler getVisionFrontEndConfig();
+ParamsHandler getCentroidalDynPreintConfig();
 
 KinDynVIO::Estimators::ProprioceptiveInput
 getProprioceptiveInputFromRobotData(const int& iter,
@@ -105,11 +154,32 @@ KinDynVIO::Estimators::IMUPreintegratorInput
 getIMUPreintegratorInputFromRobotData(const int& iter,
                                       const RobotMatData& rmd);
 
-
+Eigen::Vector3d getGravity();
 gtsam::Pose3 getBaseHCam30Degrees();
 gtsam::Pose3 getBaseHIMU();
 void getCameraIntrinsics(std::vector<double>& K,
                          std::vector<double>& gamma);
 
+void updateRobotMatBaseVelocity(RobotMatData& rmd);
+
+template <typename ArrayType, typename DataType>
+void updateLogData(const DataType& data, ArrayType& log)
+{
+    std::size_t tail;
+    if constexpr (std::is_same_v<ArrayType, Eigen::VectorXd> &&
+        std::is_same_v<DataType, double>)
+    {
+        tail = log.size() + 1;
+        log.conservativeResize(tail);
+        log(tail - 1) = data;
+    }
+    else if constexpr (std::is_same_v<ArrayType, Eigen::MatrixXd>)
+    {
+        // expects data type to be a Eigen::VectorXd or Eigen::Ref
+        tail = log.rows() + 1;
+        log.conservativeResize(tail, data.size());
+        log.row(tail - 1) = data;
+    }
+}
 
 #endif // KINDYNVIO_TEST_DATA_ESTIMATOR_UTILS_H
